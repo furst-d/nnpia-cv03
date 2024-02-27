662ae252a829012280afcd5ffc1725f6decac7e9
Dominik Fűrst <dominikfurst1@gmail.com>
2024-02-27 08:45:24 +0100

    Init

---
+++ src/main/java/org/furstd/nnpiacv03/NnpiaCv03Application.java
@@ -0,0 +1,13 @@
+package org.furstd.nnpiacv03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnpiaCv03Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/furstd/nnpiacv03/NnpiaCv03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.furstd.nnpiacv03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
